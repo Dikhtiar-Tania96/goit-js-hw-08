b845a3d2a4b14c25270d547f1a56465732675d3c
Dikhtiar-Tania96 <chygynovatania96@gmail.com>
2023-04-14 10:34:11 +0300

    commit files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+galleryEl.innerHTML = createMarkupGalleryItems(galleryItems);
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+function createMarkupGalleryItems(listItems) {
+    return listItems.map (item => 
+       ` <a class = 'gallery__item' href="${item.original}">
+            <img class = 'gallery__image' src = '${item.preview}' alt = '${item.description}' />
+        </a>)`
+    ) .join('');
+}
